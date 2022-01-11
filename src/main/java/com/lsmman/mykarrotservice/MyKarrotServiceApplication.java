456f68e6c4a695de99c1512f5815137433bc1144
lsmman <lsmman07@gmail.com>
2022-01-12 01:27:17 +0900

    init(MyKarrotService): init spring project

---
+++ src/main/java/com/lsmman/mykarrotservice/MyKarrotServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lsmman.mykarrotservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyKarrotServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyKarrotServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lsmman/mykarrotservice/MyKarrotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsmman.mykarrotservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyKarrotServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
