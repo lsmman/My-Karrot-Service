9037b00ea21c6c2ec93e012ada09cc24755e61de
lsmman <lsmman07@gmail.com>
2022-01-22 06:05:27 +0900

    feat(RegisterProductRequestDto): 상품 등록 요청 dto 구현

---
+++ src/main/java/com/lsmman/mykarrotservice/product/dto/RegisterProductRequestDto.java
@@ -0,0 +1,39 @@
+package com.lsmman.mykarrotservice.product.dto;
+
+import com.lsmman.mykarrotservice.member.Member;
+import com.lsmman.mykarrotservice.product.Category;
+import com.lsmman.mykarrotservice.product.Product;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class RegisterProductRequestDto {
+    private Member member;
+    private String filePath;
+    private String title;
+    private Category category;
+    private int price;
+    private String contents;
+
+    public static Product toProduct(RegisterProductRequestDto requestDto) {
+        return Product.builder()
+                .category(requestDto.getCategory())
+                .member(requestDto.getMember())
+                .title(requestDto.getTitle())
+                .price(requestDto.getPrice())
+                .contents(requestDto.getContents())
+                .imageUrl(requestDto.getFilePath())
+                .build();
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
