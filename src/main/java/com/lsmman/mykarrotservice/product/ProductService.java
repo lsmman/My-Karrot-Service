caf283002a1993bc3e93bc7fae123c590ce32eba
lsmman <lsmman07@gmail.com>
2022-01-22 03:42:40 +0900

    feat(ProductService): ProductService skeleton 구현

---
+++ src/main/java/com/lsmman/mykarrotservice/product/ProductService.java
@@ -0,0 +1,14 @@
+package com.lsmman.mykarrotservice.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public void createProduct(Product toProduct) {
+
+    }
+}
