fb242c602bb8c755c495707be0821c2303dd369b
lsmman <lsmman07@gmail.com>
2022-01-22 06:04:15 +0900

    feat(ProductRestApiController): createProduct 메소드 구현

--- src/main/java/com/lsmman/mykarrotservice/product/ProductRestApiController.java
+++ src/main/java/com/lsmman/mykarrotservice/product/ProductRestApiController.java
@@ -7,16 +7,25 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/product/api")
 @RequiredArgsConstructor
 public class ProductRestApiController {
     private final ProductService productService;
+    private final S3Service s3Service;
 
     @PutMapping
-    public ResponseEntity<?> createProduct(@RequestBody RegisterProductRequestDto registerProductRequestDto) {
-        productService.createProduct(RegisterProductRequestDto.toProduct(registerProductRequestDto));
+    public ResponseEntity<?> createProduct(@RequestBody RegisterProductRequestDto registerProductRequestDto,
+                                           @RequestBody MultipartFile file) throws IOException {
+
+        String imgPath = s3Service.upload(file);
+        registerProductRequestDto.setFilePath(imgPath);
+
+        productService.saveProduct(RegisterProductRequestDto.toProduct(registerProductRequestDto));
         return ResponseEntity.ok().build();
     }
 }
